a8617b58f40ed7b403e0877eaaab7711be6da1ee
Anuradha Srinivas <anuradhasrinivas@Anuradhas-MacBook-Air.local>
2025-05-20 02:39:07 +0530

    Modify routes in main page

--- frontend/skillhire-ui/src/App.js
+++ frontend/skillhire-ui/src/App.js
@@ -1,10 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-gray-50 text-gray-900">
+        <nav className="p-4 bg-white shadow mb-6 flex gap-4">
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+          <Link to="/freelancers">Freelancers</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/admin">Admin</Link>
+        </nav>
         <AppRoutes />
       </div>
     </BrowserRouter>
